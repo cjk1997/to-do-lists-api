de933c28fe76ecc7d5de1714d4d9ad5a526755b9
Carson Keller <carsonjkeller@gmail.com>
2020-05-11 14:51:01 -0600

    Created POST router in API

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -16,3 +16,13 @@ Router.get('/', async function(req, res, next) {
         res.status(500).send("Internal server issues, check logs.");
     };
 });
+
+Router.post('/', async function(req, res, next) {
+    try {
+        const data = await createList(req.body);
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal server issues, check logs.");
+    };
+});
