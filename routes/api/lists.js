3c41aec451cb7cda56ec16cb042c2cf61fc60bf6
Carson Keller <carsonjkeller@gmail.com>
2020-05-11 14:50:07 -0600

    Created GET router in API

---
+++ routes/api/lists.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Router = express.Router();
+const {
+    getLists,
+    createList,
+    addUpdateListItem,
+    deleteList    
+} = require('../../data/lists');
+
+Router.get('/', async function(req, res, next) {
+    try {
+        const data = await getLists();
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal server issues, check logs.");
+    };
+});
